4a561ff411078c4621e5f664dfbbef1feb997cd2
Cedric Hutchings <ced@misguided.enterprises>
2022-08-10 17:20:52 -0400

    color from C

--- main.c
+++ main.c
@@ -4,28 +4,39 @@
 extern float cosf(float);
 extern float sinf(float);
 
-typedef struct { float x, y; } Pos;
+typedef struct { float x, y; } Vec2;
+static Vec2 vec2_unit_from_rads(float rads) {
+  return (Vec2) { .x = cosf(rads), .y = sinf(rads) };
+}
+
+typedef struct { float r, g, b, a; } Color;
+#define COLOR_RED ((Color) { 1.0f, 0.0f, 0.0f, 1.0f })
+
+typedef struct { Vec2 pos; float _pad0, _pad1; Color color; } Vert;
+
 static struct {
-  Pos vbuf[3];
+  Vert vbuf[3];
 } state = {0};
 
 void init(void) {}
 
-Pos *vbuf(void) { return state.vbuf; }
+Vert *vbuf(void) { return state.vbuf; }
 
 void frame(int width, int height, float dt) {
-  Pos *out = state.vbuf;
+  Vert *out = state.vbuf;
 
-  *out++ = (Pos) { .x = 0.5f*cosf(dt*0.0008 + (M_PI*2.0f / 3.0f)*1.0f),
-                   .y = 0.5f*sinf(dt*0.0008 + (M_PI*2.0f / 3.0f)*1.0f) };
-  *out++ = (Pos) { .x = 0.5f*cosf(dt*0.0008 + (M_PI*2.0f / 3.0f)*2.0f),
-                   .y = 0.5f*sinf(dt*0.0008 + (M_PI*2.0f / 3.0f)*2.0f) };
-  *out++ = (Pos) { .x = 0.5f*cosf(dt*0.0008 + (M_PI*2.0f / 3.0f)*3.0f),
-                   .y = 0.5f*sinf(dt*0.0008 + (M_PI*2.0f / 3.0f)*3.0f) };
+  float rad = dt*0.0008;
+  float rad_inc = M_PI*2.0f / 3.0f;
+  *out++ = (Vert) { .pos = vec2_unit_from_rads(rad += rad_inc),
+                    .color = COLOR_RED };
+  *out++ = (Vert) { .pos = vec2_unit_from_rads(rad += rad_inc),
+                    .color = COLOR_RED };
+  *out++ = (Vert) { .pos = vec2_unit_from_rads(rad += rad_inc),
+                    .color = COLOR_RED };
 
   /* more width: show more
      more height: zoom in */
   float aspect = (float)height / (float)width;
-  for (Pos *p = state.vbuf; p != out; p++)
-    p->x *= aspect;
+  for (Vert *p = state.vbuf; p != out; p++)
+    p->pos.x *= aspect;
 }
