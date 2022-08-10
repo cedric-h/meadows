7908f7b8e82a474fb4a59575c9c849c1b74ecd81
Cedric Hutchings <ced@misguided.enterprises>
2022-08-10 17:50:47 -0400

    two tris, diff colors

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #define M_PI 3.141592653589793
+#define ARR_LEN(arr) (sizeof(arr) / sizeof(arr[0]))
 
 #define WASM_EXPORT __attribute__((visibility("default")))
 extern float cosf(float);
 extern float sinf(float);
+extern void vbuf(void *ptr, int len);
 
 typedef struct { float x, y; } Vec2;
 static Vec2 vec2_unit_from_rads(float rads) {
@@ -10,17 +12,18 @@ static Vec2 vec2_unit_from_rads(float rads) {
 }
 
 typedef struct { float r, g, b, a; } Color;
-#define COLOR_RED ((Color) { 1.0f, 0.0f, 0.0f, 1.0f })
+#define COLOR_RED  ((Color) { 1.0f, 0.0f, 0.0f, 1.0f })
+#define COLOR_BLUE ((Color) { 0.0f, 0.0f, 1.0f, 1.0f })
 
 typedef struct { Vec2 pos; float _pad0, _pad1; Color color; } Vert;
 
 static struct {
-  Vert vbuf[3];
+  Vert vbuf[6];
 } state = {0};
 
-void init(void) {}
-
-Vert *vbuf(void) { return state.vbuf; }
+void init(void) {
+  vbuf(state.vbuf, ARR_LEN(state.vbuf));
+}
 
 void frame(int width, int height, float dt) {
   Vert *out = state.vbuf;
@@ -33,6 +36,14 @@ void frame(int width, int height, float dt) {
                     .color = COLOR_RED };
   *out++ = (Vert) { .pos = vec2_unit_from_rads(rad += rad_inc),
                     .color = COLOR_RED };
+
+  rad = dt*0.0007 + rad_inc/2.0f;
+  *out++ = (Vert) { .pos = vec2_unit_from_rads(rad += rad_inc),
+                    .color = COLOR_BLUE };
+  *out++ = (Vert) { .pos = vec2_unit_from_rads(rad += rad_inc),
+                    .color = COLOR_BLUE };
+  *out++ = (Vert) { .pos = vec2_unit_from_rads(rad += rad_inc),
+                    .color = COLOR_BLUE };
 
   /* more width: show more
      more height: zoom in */
